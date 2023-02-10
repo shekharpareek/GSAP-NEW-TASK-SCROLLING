5f05b19acf156d367364ad37acc064bd09ca3b9b
shekharpareek <shekhar.pareek@sunarctechnologies.com>
2023-02-10 17:28:08 +0530

    one animation works only

--- app.js
+++ app.js
@@ -7,23 +7,37 @@ const element2 = document.querySelector("#element2");
 // Create a timeline for the animation
 const tl = gsap.timeline();
 
-// Add the animation to the timeline
-tl.to(element, {
+// Animation-1
+t1.to(element, {
   duration: 3.5, // the animation should last 1 second
-  top: -261,
-  left: -1380, // rotate the element 360 degrees along the z-axis
+  top: "-10%",
+  left: "-20%", // rotate the element 360 degrees along the z-axis
   transformOrigin: "center center", // set the rotation origin to the center of the element
   scrollTrigger: {
     trigger: ".grid-lines-bg",
-    start: "top top",
-    end: "+=200",
+    start: "100px top",
+    end: "+2500",
     scrub: true,
     pin: true,
-    ease: "slow(0.7, 0.7, false)",
+    duration: 0.5,
+    // ease: Power1.easeOut,
+    ease: Power2.easeIn,
     stagger: 2,
+    markers: true,
   },
 });
-// let verticalScrollOffset =
-//   window.pageYOffset || document.documentElement.scrollTop;
 
-// $("html, body").animate({ scrollTop: verticalScrollOffset }, "slow"); //slow Or fast option
+t1.to(".wrapper-2", {
+  duration: 3.5, // the animation should last 1 second
+  top: "0%",
+  left: "100px", // rotate the element 360 degrees along the z-axis
+  transformOrigin: "center center", // set the rotation origin to the center of the element
+  ScrollTrigger: {
+    trigger: ".grid-lines-bg",
+    start: "center center",
+    end: "3000",
+    scrub: true,
+    duration: 2,
+    markers: true,
+  },
+});
