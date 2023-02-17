2aff097ead3a6857842599be0ca926be5aaea01d
shekharpareek <shekhar.pareek@sunarctechnologies.com>
2023-02-17 18:11:32 +0530

    All changes made

--- app.js
+++ app.js
@@ -1,45 +1,85 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const wrappers = gsap.utils.toArray(".wrapper");
+const transform = gsap.utils.toArray(".transform");
+
 // First, select the element you want to animate
-const element = document.querySelector(".wrapper");
-const element2 = document.querySelector("#element2");
-let maxWidth = 0;
 // Create a timeline for the animation
-t1 = gsap.timeline({
+t1 = gsap.timeline({});
+
+// Animation - 1;
+
+gsap.to(".container", {
+  duration: 7.0,
+  x: -400,
+  y: -300,
+  transformOrigin: "center left",
   scrollTrigger: {
-    trigger: ".main-bg",
+    trigger: "container",
     start: "top",
-    end: "+2500",
-    scrub: 3,
-    pin: true,
-    duration: 0.5,
-    ease: "slow",
-    stagger: 3,
+    end: "+200",
+    scrub: true,
+    duration: 10,
+    ease: "power3.in",
+    stagger: 2,
+    toggleActions: "restart none none none",
     markers: true,
+    pin: true,
   },
 });
-// Animation-1
-t1.to(element, {
-  duration: 5.0, // the animation should last 1 second
-  top: "-12%",
-  left: "-20%", // rotate the element 360 degrees along the z-axis
-  transformOrigin: "center center", // set the rotation origin to the center of the element
-  ease: "slow",
-});
-// Animation-2
-t1.to(".transform", {
-  duration: 4.0,
-  top: "30%",
-  left: "160%",
-  transformOrigin: "left top",
-  ease: "slow",
-  opacity: 0,
-});
-// Animation-3
-t1.to(".wrapper-2", {
-  duration: 1.0,
-  left: "3%",
-  top: "6%",
-  transformOrigin: "center center",
-  ease: "slow",
-});
+// gsap.to(wrappers[0], {
+//   duration: 6.0, // the animation should last 1 second
+//   x: -950,
+//   y: -600,
+//   transformOrigin: "center center", // set the rotation origin to the center of the element
+//   ease: "slow",
+//   scrollTrigger: {
+//     trigger: ".container",
+//     start: "top center",
+//     end: "+3200",
+//     scrub: true,
+//     duration: 10,
+//     ease: "slow",
+//     stagger: 2,
+//     toggleActions: "restart none none none",
+//     markers: true,
+//     pin: true,
+//   },
+// });
+// // // Animation - 2;
+// gsap.to(wrappers[1], {
+//   duration: 15,
+//   margin: 70,
+//   y: -300,
+//   x: -100,
+//   scrollTrigger: {
+//     trigger: ".content--2",
+//     start: "top center",
+//     end: "+4000",
+//     scrub: true,
+//     duration: 10,
+//     ease: "slow",
+//     speed: "slow",
+//     stagger: 2,
+//     toggleActions: "restart none none none",
+//     markers: true,
+//     pin: true,
+//   },
+//   transformOrigin: "right top",
+// });
+// gsap.to(transform[0], {
+//   duration: 8.0,
+//   y: -500,
+//   x: 500,
+//   scrollTrigger: {
+//     trigger: "transform",
+//     start: "top center",
+//     end: "+12000",
+//     scrub: true,
+//     duration: 11,
+//     ease: "slow",
+//     stagger: 3,
+//     markers: true,
+//     pin: true,
+//   },
+// });
